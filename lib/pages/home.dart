48faf8c407acea0a605c995479f10c6fb4e1b335
Henrique Rezende <henrique.rezende667@gmail.com>
2023-10-15 15:35:21 -0300

    #4 : Criação das seções "Carreira" e "Metas", e implementação das barras de progresso para cada uma

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,11 +7,14 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+
 
   void _login() {
     Navigator.pushNamed(context, '/telaEdicao');
   }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -32,6 +35,151 @@ class _HomeState extends State<Home> {
                     IconButton(onPressed: null,
                         icon:
                         Icon(Icons.notifications, color: Colors.white, size: 30.0))
+              ]
+            ),
+            Text("Carreira",
+            style: TextStyle(color: Colors.white,
+                            fontSize: 30.0,
+                            fontWeight: FontWeight.bold)),
+            SizedBox(height: 30),
+            Container(
+              width: 370,
+              height: 80,
+              decoration: BoxDecoration(
+                color: Colors.redAccent,
+                borderRadius: BorderRadius.circular(10)
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Progresso", style: TextStyle(color: Colors.white,
+                  fontSize:17.0,
+                  fontWeight: FontWeight.bold)),
+                  SizedBox(height: 15),
+                  Container(
+                    width: 250,
+                    child: LinearProgressIndicator(
+                      backgroundColor: Colors.white,
+                      color: Color(0xFF0B235C),
+                      value: 0.5,
+                      borderRadius: BorderRadius.circular(100)
+                    ),
+                  ),
+                  
+                ],
+              ),
+            ),
+            SizedBox(height: 50),
+            Text("Metas",
+                style: TextStyle(color: Colors.white,
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold)),
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 170,
+                  height: 90,
+                  decoration: BoxDecoration(
+                      color: Colors.redAccent,
+                      borderRadius: BorderRadius.circular(10)
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Flexões", style: TextStyle(color: Colors.white,
+                          fontSize:17.0,
+                          fontWeight: FontWeight.bold)),
+                      SizedBox(height: 15),
+                      Container(
+                        width: 100,
+                        child: LinearProgressIndicator(
+                            backgroundColor: Colors.white,
+                            color: Color(0xFF0B235C),
+                            value: 0.5,
+                            borderRadius: BorderRadius.circular(100)
+                        ),
+                      ),
+                      SizedBox(height: 15),
+                      Text("15 de 20", style: TextStyle(color: Colors.white,
+                          fontSize:12.0,
+                          fontWeight: FontWeight.bold)
+
+                      ),
+
+                    ],
+                  ),
+                ),
+                SizedBox(width: 30),
+                Container(
+                  width: 170,
+                  height: 90,
+                  decoration: BoxDecoration(
+                      color: Colors.redAccent,
+                      borderRadius: BorderRadius.circular(10)
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Barras", style: TextStyle(color: Colors.white,
+                          fontSize:17.0,
+                          fontWeight: FontWeight.bold)),
+                      SizedBox(height: 15),
+                      Container(
+                        width: 100,
+                        child: LinearProgressIndicator(
+                            backgroundColor: Colors.white,
+                            color: Color(0xFF0B235C),
+                            value: 0.5,
+                            borderRadius: BorderRadius.circular(100)
+                        ),
+                      ),
+                      SizedBox(height: 15),
+                  Text("10 de 20", style: TextStyle(color: Colors.white,
+                      fontSize:12.0,
+                      fontWeight: FontWeight.bold))
+
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 170,
+                  height: 90,
+                  decoration: BoxDecoration(
+                      color: Colors.redAccent,
+                      borderRadius: BorderRadius.circular(10)
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Peso", style: TextStyle(color: Colors.white,
+                          fontSize:17.0,
+                          fontWeight: FontWeight.bold)),
+                      SizedBox(height: 15),
+                      Container(
+                        width: 100,
+                        child: LinearProgressIndicator(
+                            backgroundColor: Colors.white,
+                            color: Color(0xFF0B235C),
+                            value: 0.5,
+                            borderRadius: BorderRadius.circular(100),
+                        ),
+                      ),
+                      SizedBox(height: 15),
+                  Text("5kgs de 10", style: TextStyle(color: Colors.white,
+                      fontSize:12.0,
+                      fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                ),
+
               ],
             )
           ],
