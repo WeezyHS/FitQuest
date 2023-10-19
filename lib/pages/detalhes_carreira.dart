8c2b14e590bd43a12b2f3faaf8c08b09137bf604
Henrique Rezende <henrique.rezende667@gmail.com>
2023-10-19 10:02:59 -0300

    #9 : Construção da estrutura da tela de detalhes da carreira

--- lib/pages/detalhes_carreira.dart
+++ lib/pages/detalhes_carreira.dart
@@ -15,6 +15,198 @@ class _detalhes_carreiraState extends State<detalhes_carreira> {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Color(0xFF0B235C),
+        body: SingleChildScrollView(
+          child: Container(
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+
+                  SizedBox(height: 50),
+                  Text("Sua Carreira",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.bold
+                    )),
+
+                SizedBox(height: 50),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('assets/images/escudo.png'),
+                            fit: BoxFit.cover
+                        )
+                    ),
+                  ),
+
+                SizedBox(height: 30),
+                Text("Nível 5",
+                    style: TextStyle(
+                        color: Color(0xFFbbb6b5),
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.bold
+                    )),
+
+                SizedBox(height: 20),
+                Container(
+                  width: 300,
+                  height: 10,
+                  child: LinearProgressIndicator(
+                      backgroundColor: Colors.white,
+                      color: Color(0xFFbbb6b5),
+                      value: 0.5,
+                      borderRadius: BorderRadius.circular(100)
+                  ),
+                ),
+
+
+                SizedBox(height: 50),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+
+                    Container(
+                      width: 180,
+                      height: 250,
+                      decoration: BoxDecoration(
+                        color: Colors.redAccent,
+                        borderRadius: BorderRadius.circular(10)
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            height: 70,
+                            width: 70,
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: AssetImage('assets/images/push_up.png'),
+                                    fit: BoxFit.cover
+                                )
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text("15/",
+                                style: TextStyle(
+                                  fontSize: 30.0,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold
+                                )),
+                              Text("20",
+                                  style: TextStyle(
+                                      fontSize: 15.0,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold
+                                  )),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    SizedBox(width: 10),
+                    Container(
+                      width: 180,
+                      height: 250,
+                      decoration: BoxDecoration(
+                          color: Colors.redAccent,
+                          borderRadius: BorderRadius.circular(10)
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            height: 60,
+                            width: 60,
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: AssetImage('assets/images/pull_up.png'),
+                                    fit: BoxFit.cover
+                                )
+                            ),
+                          ),
+                          SizedBox(height: 15),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text("15/",
+                                  style: TextStyle(
+                                      fontSize: 30.0,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold
+                                  )),
+                              Text("20",
+                                  style: TextStyle(
+                                      fontSize: 15.0,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold
+                                  )),
+                            ],
+                          ),
+                        ],
+                      ),
+                      ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 370,
+                      height: 150,
+                      decoration: BoxDecoration(
+                          color: Colors.redAccent,
+                          borderRadius: BorderRadius.circular(10)
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            height: 60,
+                            width: 60,
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: AssetImage('assets/images/bar.png'),
+                                    fit: BoxFit.cover
+                                )
+                            ),
+                          ),
+                          SizedBox(height: 15),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text("15/",
+                                  style: TextStyle(
+                                      fontSize: 30.0,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold
+                                  )),
+                              Text("20",
+                                  style: TextStyle(
+                                      fontSize: 15.0,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold
+                                  )),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+
+
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
