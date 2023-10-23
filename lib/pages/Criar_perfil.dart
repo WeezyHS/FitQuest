062620fca064a26bc5b4ddb9220aa3c3a54361ba
wesley.henderson <wesley.henderson@rede.ulbra.br>
2023-10-23 09:21:12 -0300

    Tela de criação de perfil

---
+++ lib/pages/Criar_perfil.dart
@@ -0,0 +1,188 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class Criar_perfil extends StatefulWidget {
+  const Criar_perfil({super.key});
+
+  @override
+  State<Criar_perfil> createState() => _CriarPerfilState();
+}
+
+class _CriarPerfilState extends State<Criar_perfil> {
+  final TextEditingController _heightController = TextEditingController();
+  final TextEditingController _weightController = TextEditingController();
+  final TextEditingController _sexController = TextEditingController();
+  String selectedGender = 'Feminino';
+
+  bool isButtonEnabled = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _heightController.addListener(_updateButtonState);
+    _weightController.addListener(_updateButtonState);
+    _sexController.addListener(_updateButtonState);
+  }
+
+  void _updateButtonState() {
+    final sex = _sexController.text;
+  }
+
+  void _login() {
+
+    if (isButtonEnabled) {
+      Navigator.pushNamed(context, '/home');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xFF0B235C),
+        body: ListView(
+          padding: EdgeInsets.all(16.0),
+          children: <Widget>[
+            SvgPicture.asset('assets/images/logo.svg', height: 350),
+            Center(
+              child: Text(
+                "Crie seu Perfil",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontSize: 30,
+                ),
+              ),
+            ),
+            SizedBox(height: 50),
+            TextFormField(
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                hintText: "Seu Nick:",
+                hintStyle: TextStyle(color: Colors.blueGrey),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                  borderSide: BorderSide(
+                    color: Colors.redAccent,
+                    width: 2.5,
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                  borderSide: BorderSide(
+                    color: Colors.redAccent,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+            TextFormField(
+              style: TextStyle(color: Colors.white),
+              //controller: _emailController,
+              decoration: InputDecoration(
+                hintText: "Idade:",
+                hintStyle: TextStyle(color: Colors.blueGrey),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                  borderSide: BorderSide(
+                    color: Colors.redAccent,
+                    width: 2.5,
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                  borderSide: BorderSide(
+                    color: Colors.redAccent,
+                  ),
+                ),
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            SizedBox(height: 16),
+            TextFormField(
+              style: TextStyle(color: Colors.white),
+              controller: _heightController,
+              decoration: InputDecoration(
+                hintText: "Altura:",
+                hintStyle: TextStyle(color: Colors.blueGrey),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                  borderSide: BorderSide(
+                    color: Colors.redAccent,
+                    width: 2.5,
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                  borderSide: BorderSide(
+                    color: Colors.redAccent,
+                  ),
+                ),
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            SizedBox(height: 16),
+            TextFormField(
+              style: TextStyle(color: Colors.white),
+              controller: _weightController,
+              decoration: InputDecoration(
+                hintText: "Peso:",
+                hintStyle: TextStyle(color: Colors.blueGrey),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                  borderSide: BorderSide(
+                    color: Colors.redAccent,
+                    width: 2.5,
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                  borderSide: BorderSide(
+                    color: Colors.redAccent,
+                  ),
+                ),
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            SizedBox(height: 16),
+            TextFormField(
+              style: TextStyle(color: Colors.white),
+              controller: _sexController,
+              decoration: InputDecoration(
+                hintText: "Sexo:",
+                hintStyle: TextStyle(color: Colors.blueGrey),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                  borderSide: BorderSide(
+                    color: Colors.redAccent,
+                    width: 2.5,
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                  borderSide: BorderSide(
+                    color: Colors.redAccent,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 32),
+            Center(
+              child: ElevatedButton(
+                onPressed: isButtonEnabled ? _login : null,
+                child: Text('Criar Perfil'),
+                style: TextButton.styleFrom(
+                  backgroundColor: isButtonEnabled ? Colors.redAccent : Colors.grey,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  padding: EdgeInsets.all(20),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
